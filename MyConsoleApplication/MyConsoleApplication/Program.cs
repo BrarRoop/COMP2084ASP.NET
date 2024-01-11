6fafe29a7d5d9f43ca5bf979d1c55ea7e99d6fd8
BrarRoop <brarroopkaran@gmail.com>
2024-01-11 12:11:10 -0500

    created new application

---
+++ MyConsoleApplication/MyConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub Desktop");
+        }
+    }
+}
