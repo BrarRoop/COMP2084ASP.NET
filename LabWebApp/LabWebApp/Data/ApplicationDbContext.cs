aa254c1e9a49279f085e508d9947044582aeca91
BrarRoop <brarroopkaran@gmail.com>
2024-03-17 14:46:56 +0300

    Identity aunthentication

--- LabWebApp/LabWebApp/Data/ApplicationDbContext.cs
+++ LabWebApp/LabWebApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using LabWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LabWebApp.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- LabWebApp/LabWebApp/Program.cs
+++ LabWebApp/LabWebApp/Program.cs
@@ -1,12 +1,57 @@
 using LabWebApp.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+ConfigurationManager configuration = builder.Configuration;
+// Step 1. Add Authentication Mechanisms
+services
+    .AddAuthentication(o =>
+    {
+        o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(o =>
+    {
+        // set the path for the authentication challenge
+        o.LoginPath = "/signin";
+        // set the path for the sign out
+        o.LogoutPath = "/signout";
+    })
+    .AddGitHub(o =>
+    {
+        o.ClientId = configuration["Authentication:GitHub:clientId"];
+        o.ClientSecret = configuration["Authentication:GitHub:clientSecret"];
+        o.CallbackPath = "/signin-github";
+
+        // Grants access to read a user's profile data.
+        // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+        o.Scope.Add("read:user");
+
+        // Optional
+        // if you need an access token to call GitHub Apis
+        o.Events.OnCreatingTicket += context =>
+        {
+            if (context.AccessToken is { })
+            {
+                context.Identity?.AddClaim(new Claim("access_token", context.AccessToken));
+            }
+
+            return Task.CompletedTask;
+        };
+    });
+
+services.AddRazorPages();
+//end addition
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("ProductDBConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+//Configure Identity
+Identity: builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 //Connections Stories
@@ -24,6 +69,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+// Add authentication middleware
+app.UseAuthentication();
 
 app.UseAuthorization();
 
